96fa6468880fff0e685445c77b64df234a0af76c
castlest <castlest@gmail.com>
2015-05-07 17:59:29 -0400

    Add JavaScript function to allow dynamically changing input fields.

---
+++ research-symposium/abstract-submission/dynamicInput.js
@@ -0,0 +1,27 @@
+// dynamicInput.js
+//
+// Allow html forms to include text input fields which can optionally
+// provide additional input fields.
+// The result is stored in an array from the form.
+//
+// Author: S.T. Castle
+// Created: 07 May 2015
+// From: Allen Liu, www.randomsnippets.com
+
+// Track the current number of fields added.
+var counter = 1;
+// Place a limit on the total number of fields allowed.
+var limit = 3;
+// Main function.
+function addInput(divName){
+    if (counter == limit)  {
+        alert("You have reached the limit of adding " + counter + " inputs");
+    }
+    else {
+        var newdiv = document.createElement('div');
+        newdiv.innerHTML = "Author " + (counter + 2) +
+                        " <br><input type='text' name='myInputs[]'>";
+        document.getElementById(divName).appendChild(newdiv);
+        counter++;
+    }
+}
